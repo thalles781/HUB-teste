6e16e07d1fb4ecc0b9e17b611279b68e3e2112be
Talos <Talos>
2025-09-17 20:54:20 -0300

    update brook.lua

--- brook.lua
+++ brook.lua
@@ -10,7 +10,7 @@ local Window = Rayfield:CreateWindow({
     ConfigurationSaving = {Enabled = true, FolderName = nil, FileName = "Talos Hub"},
     Discord = {Enabled = false, Invite = "JF2F2RANud", RememberJoins = true},
     
-    KeySystem = true,
+    KeySystem = false,
     KeySettings = {
       Title = "Talos Key",
       Subtitle = "Key System",
